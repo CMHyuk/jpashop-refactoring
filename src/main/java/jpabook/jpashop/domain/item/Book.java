5360941efc998214cffc7ba52661e8f4cac6ad4d
CMHyuk <alsgur9710@naver.com>
2023-04-12 22:42:56 +0900

    refactor : 빌더 적용

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,18 +1,26 @@
 package jpabook.jpashop.domain.item;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @DiscriminatorValue("B")
 @Getter
 @Setter
+@NoArgsConstructor(access = PROTECTED)
 public class Book extends Item {
 
     private String author;
     private String isbn;
 
+    @Builder
+    public Book(String name, int price, int stockQuantity, String author, String isbn) {
+        super(name, price, stockQuantity);
+        this.author = author;
+        this.isbn = isbn;
+    }
 }
