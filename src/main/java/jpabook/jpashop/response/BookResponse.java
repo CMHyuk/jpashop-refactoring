a874c1357eb1c665c20cefec9e1ae25ebcbbf90b
CMHyuk <alsgur9710@naver.com>
2023-04-13 20:33:02 +0900

    feat : 책 dto

---
+++ src/main/java/jpabook/jpashop/response/BookResponse.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.response;
+
+import lombok.Getter;
+
+@Getter
+public class BookResponse extends ItemResponse {
+
+    private String author;
+    private String isbn;
+
+    public BookResponse(ItemResponse item) {
+        super(item.getItemId(), item.getName(), item.getPrice(), item.getStockQuantity());
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
