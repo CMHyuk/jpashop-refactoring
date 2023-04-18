78bd7bf17101669804ed08157214a28c811dcf1a
CMHyuk <alsgur9710@naver.com>
2023-04-18 14:58:54 +0900

    refactor : 빌더 적용

--- src/main/java/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.controller;
 
+import jpabook.jpashop.response.BookResponse;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@NoArgsConstructor
 public class BookForm {
 
     private Long id;
@@ -14,4 +18,25 @@ public class BookForm {
 
     private String author;
     private String isbn;
+
+    public static BookForm toBook(BookResponse bookResponse) {
+        return BookForm.builder()
+                .id(bookResponse.getItemId())
+                .name(bookResponse.getName())
+                .price(bookResponse.getPrice())
+                .stockQuantity(bookResponse.getStockQuantity())
+                .author(bookResponse.getAuthor())
+                .isbn(bookResponse.getIsbn())
+                .build();
+    }
+
+    @Builder
+    public BookForm(Long id, String name, int price, int stockQuantity, String author, String isbn) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.author = author;
+        this.isbn = isbn;
+    }
 }
