b25ce6514a95b1e828e81e40e92454e104cecdc8
CMHyuk <alsgur9710@naver.com>
2023-04-23 13:41:25 +0900

    feat : 반환 메서드

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.controller.BookForm;
 import lombok.*;
 
 import javax.persistence.DiscriminatorValue;
@@ -17,10 +18,27 @@ public class Book extends Item {
     private String author;
     private String isbn;
 
+    public static Book build(BookForm form) {
+        return Book.builder()
+                .name(form.getName())
+                .price(form.getPrice())
+                .stockQuantity(form.getStockQuantity())
+                .author(form.getAuthor())
+                .isbn(form.getIsbn())
+                .build();
+    }
+
     @Builder
     public Book(String name, int price, int stockQuantity, String author, String isbn) {
         super(name, price, stockQuantity);
         this.author = author;
         this.isbn = isbn;
+    }
+
+    @Override
+    public void updateItem(BookForm form) {
+        super.updateItem(form);
+        this.setAuthor(form.getAuthor());
+        this.setIsbn(form.getIsbn());
     }
 }
